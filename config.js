7250647c10aa71f60d6e9c94b3d0941bed51c2fe
arianazuniga <39392083+arianazuniga@users.noreply.github.com>
2018-12-05 01:20:57 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports={
+	port:3306,
+	db:{
+		database: 'dbalmacen',
+		user: 'ArianaZuniga',
+		password:'ArianaZuniga',
+		options:{
+			host:'mydbalmacen.coqbcv5ezi7s.us-east-2.rds.amazonaws.com',
+			dialect:'mysql',
+			pool:{
+				maxConnections:5,
+				minConnections:0,
+				maxIdleTime:1000
+			}
+		}
+	}
+}
